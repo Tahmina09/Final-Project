b43acfdd12778ecdd2ebb3e2f2636ce6b2452df6
Тakhmina <tahminakhalilova9@gmail.com>
2022-10-16 16:26:01 +0500

    Делаем массив как можно резиновым

---
+++ project/Program.cs
@@ -0,0 +1,7 @@
+Console. Clear ();
+Console.WriteLine("Сколько строк хотите ввести?");
+int count = Convert. ToInt32 (Console. ReadLine ());
+string [] array = new string [count]; 
+string str = String. Empty;
+Console.WriteLine();
+
